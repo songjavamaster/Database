b1187aa458239d162996f78558ea8ca1cd795a66
thddlswo2 <thddlswo2@naver.com>
2022-07-29 17:59:04 +0900

    db.sql

---
+++ query.sql
@@ -0,0 +1,12 @@
+INSERT INTO buytblbuytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('KHD', '운동화', NULL, 480, 2);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('KHD', '노트북', '전자', 16000, 1);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('KYM', '모니터', '전자', 3200, 1);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('PSH', '모니터', '전자', 3200, 5);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('KHD', '청바지', '의류', 800, 3);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('PSH', '메모리', '전자', 1280, 10);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('KJD', '책', '서적', 240, 5);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('LHJ', '책', '서적', 240, 2);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('LHJ', '청바지', '의류', 800, 1);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('PSH', '운동화', NULL, 480, 2);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('LHJ', '책', '서적', 240, 1);
+INSERT INTO buytbl (`userID`, `prodName`, `groupName`, `price`, `amount`) VALUES ('PSH', '운동화', NULL, 480, 2);
